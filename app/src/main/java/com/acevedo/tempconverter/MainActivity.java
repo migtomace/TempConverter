fa087e9870440c23c14fde8aaf5227b24a78fcc7
migtomace <32564100+migtomace@users.noreply.github.com>
2018-01-31 17:14:09 -0800

    outputs to CTextView

--- app/src/main/java/com/acevedo/tempconverter/MainActivity.java
+++ app/src/main/java/com/acevedo/tempconverter/MainActivity.java
@@ -2,12 +2,64 @@ package com.acevedo.tempconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity implements TextView.OnEditorActionListener {
+
+    //define variables for widgets
+    private EditText FText;
+    private TextView CTextView;
+
+
+    private String fString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get references to the widgets
+        CTextView = (TextView) findViewById(R.id.C);
+        FText = (EditText) findViewById(R.id.F);
+
+        FText.setOnEditorActionListener(this);
+
+}
+
+    @Override
+    public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+        calculateAndDisplay();
+        return false;
     }
+
+    private void calculateAndDisplay(){
+        //create string
+        fString = FText.getText().toString();
+
+        float c;
+        float F;
+        
+
+        try {
+            F = Float.parseFloat(fString);
+        }catch (NumberFormatException e){
+            F = 0;
+        }
+
+
+        c = (F-32) * 5/9;
+
+        //display celsius
+
+        CTextView.setText(String.valueOf(c));
+
+    }
+
 }
